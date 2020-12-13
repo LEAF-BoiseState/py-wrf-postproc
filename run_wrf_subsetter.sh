dd41a0475e7654f8b5842b25e6591a80c89cc346
Lejo <lejoflores@boisestate.edu>
2020-12-12 21:32:00 -0700

    Fix issue with env variable

--- run_wrf_subsetter.sh
+++ run_wrf_subsetter.sh
@@ -13,7 +13,7 @@ export subset_expr=$5
 export daily_path=/home/aflores/scratch/wrf_30yr_daily_summaries
 
 
-mkdir subset_path
-mkdir daily_path
+mkdir ${subset_path}
+mkdir ${daily_path}
 
 python3 wrf-subsetter.py ${CreateDailySummary} ${wrfout_path} ${wrfout_expr} ${subset_path} ${subset_expr} ${daily_path} ${yaml_file}
