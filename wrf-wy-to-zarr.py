afd865c16f74933fa39155c4cc4bb4b0982f8438
Lejo <lejoflores@boisestate.edu>
2021-02-27 13:43:04 -0700

    fixed error in function definition

--- wrf-wy-to-zarr.py
+++ wrf-wy-to-zarr.py
@@ -6,6 +6,16 @@ subset_key = 'forcing'
 base_dir = '/home/aflores/scratch/wrf_30yr_subsets/forcing/'
 
 
+def nc_to_zarr(wy_ss_dir, zarr_store):
+
+    ds = xr.open_mfdataset(wy_ss_dir+'/*.nc', concat_dim='Time', combine='nested')
+
+    ds.to_zarr(zarr_store, consolidated=True)
+
+    print('Completed output to '+zarr_store)
+
+    return
+
 for year in np.arange(1988,2018):
 
     wy_ss_dir = base_dir+'wy'+'{:d}'.format(year)
@@ -18,12 +28,3 @@ for year in np.arange(1988,2018):
     nc_to_zarr(wy_ss_dir, zarr_store)
 
 
-def nc_to_zarr(wy_ss_dir, zarr_store):
-
-    ds = xr.open_mfdataset(wy_ss_dir+'/*.nc', concat_dim='Time', combine='nested')
-
-    ds.to_zarr(zarr_store, consolidated=True)
-
-    print('Completed output to '+zarr_store)
-
-    return
